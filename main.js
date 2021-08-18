2c81d9d436c6e60ba6f185587e4824661db73dbb
Pierre Portejoie <portejoiepierre@gmail.com>
2021-08-18 10:08:53 +0200

    clean code + setup tracking for all elements

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
-  const aff = localStorage.getItem("aff") || ""
+  // SETUP LINK
   const deviceId =
     localStorage.getItem("deviceId") ??
     `${Math.floor(10000 * (new Date().valueOf() + Math.random()))}`
@@ -14,52 +14,38 @@ setTimeout(() => {
     deviceId +
     "&ga=" +
     clientId
-  document.getElementById("add-to-chrome-navbar").href = link
-  document.getElementById("add-to-chrome-navbar").onclick = goToChromeStoreClick
-  document.getElementById("add-to-chrome-hero").href = link
-  document.getElementById("add-to-chrome-hero").onclick = goToChromeStoreClick
-  document.getElementById("add-to-chrome-footer").href = link
-  document.getElementById("add-to-chrome-footer").onclick = goToChromeStoreClick
 
-  function goToChromeStoreClick() {
-    var headers = new Headers()
-    headers.append("Content-Type", "application/json")
-
-    var raw = JSON.stringify({ type: "GO_TO_CHROME_STORE", deviceId: deviceId })
-
-    var requestOptions = {
-      method: "POST",
-      headers: headers,
-      body: raw,
-      redirect: "follow",
-    }
-
-    fetch("https://api.leadjet.io/pevent", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error))
-  }
-
-  // CTA  test
-  console.log(
-    google_optimize && google_optimize.get("<M0X6xkCPQR-5SiNYAmdBoQ>")
-  )
-
-  function implementExperimentA(value) {
-    if (value == "0") {
-      console.log(0)
-    } else if (value == "1") {
-      console.log(1)
-    } else if (value == "2") {
-      console.log(2)
-    }
-  }
-
-  gtag("event", "optimize.callback", {
-    name: "<M0X6xkCPQR-5SiNYAmdBoQ>",
-    callback: implementExperimentA,
-  })
+  // SETUP CTAs
+  document
+    .querySelectorAll(
+      "#add-to-chrome-navbar, #add-to-chrome-hero, #add-to-chrome-footer"
+    )
+    .forEach((e) => {
+      e.href = link
+      e.onclick = goToChromeStoreClick
+    })
 }, 500)
+
+function goToChromeStoreClick() {
+  var headers = new Headers()
+  headers.append("Content-Type", "application/json")
+
+  var raw = JSON.stringify({ type: "GO_TO_CHROME_STORE", deviceId: deviceId })
+
+  var requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: raw,
+    redirect: "follow",
+  }
+
+  fetch("https://api.leadjet.io/pevent", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error))
+}
+
+// VIDEO AUTOPLAY
 ;(function (window) {
   var playing = false
 
